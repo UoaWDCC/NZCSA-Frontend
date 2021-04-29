0c07a77c752ef5fcac591ae90a1c22145d74675e
Tony Cui <Tonycui02@gmail.com>
2021-04-29 23:02:29 +1200

    Initial Login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SignInSide from "./pages/SignInSide";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/dashboard/Dashboard";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import purple from "@material-ui/core/colors/purple";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[500],
+    },
+    secondary: {
+      main: purple[500],
+    },
+  },
+});
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signOut(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return fakeAuth.isAuthenticated === true ? (
+          children
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://nzcsa.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NZCSA
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <SignInSide />
+              </Route>
+              <Route path="/login">
+                <SignInSide />
+              </Route>
+              <Route path="/signup">
+                <SignUp />
+              </Route>
+              <PrivateRoute path="/dashboard">
+                <Dashboard />
+              </PrivateRoute>
+            </Switch>
+          </Router>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
