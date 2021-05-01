c8b755d47fa8ba3513c2880bbc670a2e25e097b9
Tony Cui <Tonycui02@gmail.com>
2021-05-01 18:13:41 +1200

    initial

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,12 +4,14 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
+// TODO: Modify to match figma design
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
   },
 }));
 
+// TODO: Modify to match figma design
 export default function Dashboard() {
   const classes = useStyles();
 
