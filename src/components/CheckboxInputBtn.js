8b5aeaa2c8c442ce4f086fbaff546470549a4e55
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2021-07-04 15:07:14 +1200

    Nzcsa 028/implement upgrade form (#14)
    * Set up the registration form
    
    * Re-align all components
    
    * Rearangement and year validation
    
    * birthday validation
    
    * Radio button with input
    
    * Implement the radio input button
    
    * implemented select function for faculty
    
    * implemented checkbox with input button
    
    * Added the checkboxInputBtn and getAllFaculty()
    
    Filters the selected faculty
    
    * input handling
    
    * closing and sumbit upgrade form
    
    Alert box shown when closing the form and enabled close on submission
    
    * changed year input to type text

---
+++ src/components/CheckboxInputBtn.js
@@ -0,0 +1,29 @@
+import React, { useState, useRef } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+export default function CheckboxInputBtn(props) {
+    const [other,setOther]=useState("Others")
+    const handleOther=(e)=>{
+        props.setOtherOption(e.target.value)
+    }
+
+    const handleOnClick=()=>{
+        props.setOption({...props.options,Others:true})
+    }
+    
+    return (
+        <FormControlLabel
+            control={<Checkbox
+                checked={props.options.Others}
+                name="Others"
+            onChange={(event)=>{props.setOption({...props.options,[event.target.name]:event.target.checked})}}
+            />}
+            label={<TextField label="Others" 
+            onClick={handleOnClick}
+            onChange={handleOther}
+            />}
+        />
+    )
+}
---
+++ src/components/RadioIInputBtn.js
@@ -0,0 +1,29 @@
+import React, { useState,useRef } from 'react';
+import Radio from '@material-ui/core/Radio';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
+export default function RadioInputBtn(props) {
+    const[other,setOtherValue]=useState('other');
+    const handleOther=(e)=>{
+        props.setOther(e.target.value)
+        setOtherValue(e.target.value);
+    }
+
+    return (
+        <FormControlLabel
+            value={other}
+            control={<Radio />}
+            label={
+                <TextField
+                    required
+                    id={'otherTextfield'}
+                    label="other"
+                    onChange={handleOther} 
+                    onClick={()=>{
+                        props.setOther(other)
+                    }}/>}
+        />
+    )
+}
