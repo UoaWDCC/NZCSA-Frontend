5a11c410cb0d8c9c0469e60d56358aae423765ac
MeloGuan <meloguan123@gmail.com>
2021-07-14 22:22:15 +1200

    fixed

--- src/api/connectBackend.js
+++ src/api/connectBackend.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function signUp(signUpInfo) {
   const body = signUpInfo;
-  const response = await axios("http://localhost:5000/api/auth/register", {
+  const response = await axios("https://nzcsa-backend.herokuapp.com/api/auth/register", {
     headers: {
       "Content-type": "application/json",
     },
