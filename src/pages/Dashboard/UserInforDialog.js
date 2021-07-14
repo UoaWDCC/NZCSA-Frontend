c1b1769d9348e3ee3ff7155395afa936d4f09317
Kirsty Gong <kirstygong23@gmail.com>
2021-07-14 17:39:51 +1200

    user information dialog created

---
+++ src/pages/Dashboard/UserInforDialog.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function UserInforDialog(props) {
+    return (
+        <Dialog
+            open={props.open}
+            onClose={props.close}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Let Google help apps determine location. This means sending anonymous location data to
+                    Google, even when no apps are running.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            </DialogActions>
+        </Dialog>
+    )
+}
