390a29ba51b7ff138590861778303cb2b4e74721
MeloGuan <meloguan123@gmail.com>
2021-07-16 23:08:41 +1200

    added loading timeout at home page

--- src/pages/Dashboard/EventGrid.js
+++ src/pages/Dashboard/EventGrid.js
@@ -17,11 +17,19 @@ export default function AltCard(props) {
     const classes = useStyles()
 
     const [userEventsDetail, setUserEventsDetail] = useState({});
-    
+    const [activeEventsDetail, setActiveEventsDetail] = useState({});
+
     useEffect(() => {
         if (props.userData == undefined) {
+            let eventBufferList = {}
+            Object.keys(props.data).forEach((key) => {
+                if (props.data[key].isActive) {
+                    eventBufferList[key] = props.data[key];
+                }
+            })
+            setActiveEventsDetail(eventBufferList);
             setUserEventsDetail(props.data);
-        }else{
+        } else {
             if ((!!props.data) && (Object.keys(props.data).length != 0) && (!!props.userData) && (Object.keys(props.userData).length != 0)) {
                 let userEvents = {}
                 if (Object.keys(props.userData).length !== 0) {
@@ -29,34 +37,52 @@ export default function AltCard(props) {
                         const eventId = props.userData.attendedEvents[i]
                         userEvents[i] = props.data[eventId]
                         // console.log(props.userData.attendedEvents[i])
-    
+
                     }
                 }
                 //console.log(userEvents);
                 setUserEventsDetail(userEvents);
             }
-        }  
-    },[props.data, props.userData])
-    
-   
+        }
+    }, [props.data, props.userData])
+
+
     //console.log(userEventsDetail)
 
     return (
-        <div className={classes.root}>
-            <Grid
-                container
-                spacing={2}
-                direction="row"
-                justify="flex-start"
-                alignItems="flex-start"
-            >
-                {Object.keys(userEventsDetail).map((elem, i) => (
-                    <Grid item xs={12} sm={6} md={3} key={userEventsDetail[elem].id}>
-                        <EventCard id={userEventsDetail[elem]._id} title={userEventsDetail[elem].eventName} date={userEventsDetail[elem].eventTime} location={userEventsDetail[elem].eventLocation} image={userEventsDetail[elem].eventImgUrl} isMember={props.isMember} price={userEventsDetail[elem].eventPrice} attendedEvents={props.attendedEvents} isYourPage={props.userData}/>
+        props.userData == undefined ?
+            (<div className={classes.root}>
+                <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="flex-start"
+                >
+                    {Object.keys(activeEventsDetail).map((elem, i) => (
+                        <Grid item xs={12} sm={6} md={3} key={activeEventsDetail[elem].id}>
+                            <EventCard id={activeEventsDetail[elem]._id} title={activeEventsDetail[elem].eventName} date={activeEventsDetail[elem].eventTime} location={activeEventsDetail[elem].eventLocation} image={activeEventsDetail[elem].eventImgUrl} isMember={props.isMember} price={activeEventsDetail[elem].eventPrice} attendedEvents={props.attendedEvents} isYourPage={props.userData} />
 
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>)
+            : (<div className={classes.root}>
+                <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="flex-start"
+                >
+                    {Object.keys(userEventsDetail).map((elem, i) => (
+                        <Grid item xs={12} sm={6} md={3} key={userEventsDetail[elem].id}>
+                            <EventCard id={userEventsDetail[elem]._id} title={userEventsDetail[elem].eventName} date={userEventsDetail[elem].eventTime} location={userEventsDetail[elem].eventLocation} image={userEventsDetail[elem].eventImgUrl} isMember={props.isMember} price={userEventsDetail[elem].eventPrice} attendedEvents={props.attendedEvents} isYourPage={props.userData} />
+
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>)
+
     )
 }
