bd650ddf7ed279cd593a35a251449490a21836b5
Kirsty Gong <kirstygong23@gmail.com>
2021-09-29 14:31:17 +1300

    fix: fix radioInput and CheckboxInput

--- src/components/formGenerator/elements/Inputfield.js
+++ src/components/formGenerator/elements/Inputfield.js
@@ -2,11 +2,5 @@ import { FormControlLabel } from "@material-ui/core";
 import React from "react";
 
 export default function Inputfield(props) {
-  return (
-    <FormControlLabel
-      ontrol={<Checkbox />}
-      label={props.label}
-      Multiline={props.Multiline}
-    />
-  );
+  return <TextField id="textField" label="Inputfiel" variant="outlined" />;
 }
--- src/components/formGenerator/elements/RadioInput.js
+++ src/components/formGenerator/elements/RadioInput.js
@@ -1,12 +1,25 @@
-import { FormControlLabel } from "@material-ui/core";
+import { FormControlLabel, RadioGroup, Radio } from "@material-ui/core";
 import React from "react";
 
 export default function RadioInput(props) {
   return (
-    <FormControlLabel
-      value={props.value}
-      control={<Radio />}
-      label={props.label}
-    />
+    <FormControl>
+      <FormLabel component="legend">{props.optionName}</FormLabel>
+      <RadioGroup
+        row
+        aria-label={props.optionName}
+        name="row-radio-buttons-group"
+      >
+        {props.fieldOption.length > 0 &&
+          props.fieldOption.map((option, i) => {
+            <FormControlLabel
+              key={i}
+              value={option}
+              control={<Radio />}
+              label={option}
+            />;
+          })}
+      </RadioGroup>
+    </FormControl>
   );
 }
