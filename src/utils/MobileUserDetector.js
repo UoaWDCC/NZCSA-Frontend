337796fbb1b41f347d04c639a32924ffbdc92fb5
Kirsty Gong <kirstygong23@gmail.com>
2021-12-27 13:56:00 +1300

    feat: detect mobile user

---
+++ src/utils/MobileUserDetector.js
@@ -0,0 +1,8 @@
+export default function mobileUserDetector() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
