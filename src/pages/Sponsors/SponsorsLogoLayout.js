6abb9863d6697e6b8dde64a992c97f8717a26ed0
Kirsty Gong <kirstygong23@gmail.com>
2022-03-26 19:30:15 +1300

    feat: reorganize special sponsors

--- src/pages/Sponsors/SponsorsLogoLayout.js
+++ src/pages/Sponsors/SponsorsLogoLayout.js
@@ -1,97 +1,116 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import {
-    Grid,
-    Paper,
-    Box
-} from '@material-ui/core/';
-import images from './SponsorsLogoList';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, Box } from "@material-ui/core/";
+import images from "./SponsorsLogoList";
 import Typography from "@material-ui/core/Typography";
 
-
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        padding: theme.spacing(2)
-    },
-    images: {
-        width: '60%',
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: "flex",
-        overflow: "auto",
-        flexDirection: "column",
-    }
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+  images: {
+    width: "60%",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
 }));
 
 export default function SponsorsLogoLayout() {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <Grid container
-                spacing={0}
-                direction="row"
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6" align="center" variant="h5">
+              Our Valuable Sponsors
+            </Typography>
+          </Paper>
+        </Grid>
+        {images
+          .filter((image) => image.description === "valuable")
+          .map(({ id, src, title, description }) => (
+            <Grid item xs={12} sm={4}>
+              <Box
+                display="flex"
                 justifyContent="center"
                 alignItems="center"
-                style={{ minHeight: '100vh' }}
-            >
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <Typography variant="h6" align='center' variant="h5" >Our Valuable Sponsors</Typography>
-                    </Paper>
-                </Grid>
-                {images.filter(image => image.description === "valuable").map(({ id, src, title, description }) => (
-                    <Grid item xs={12} sm={4}>
-                        <Box
-                            display="flex"
-                            justifyContent="center"
-                            alignItems="center"
-                            minHeight="20vh"
-                        >
-                            <img key={id} src={src} alt="Logo" className={classes.images}  />
-                        </Box>
-                    </Grid>
-                ))}
-
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <Typography variant="h6" align='center' variant="h5" >Our Contributory Sponsors</Typography>
-                    </Paper>
-                </Grid>
-                {images.filter(image => image.description === "contributory").map(({ id, src, title, description }) => (
-                    <Grid item xs={12} sm={4}>
-                        <Box
-                            display="flex"
-                            justifyContent="center"
-                            alignItems="center"
-                            minHeight="20vh"
-                        >
-                            <img key={id} src={src} alt="Logo" className={classes.images} />
-                        </Box>
-                    </Grid>
-                ))}
-
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <Typography variant="h6" align='center' variant="h5" >Our Special Sponsors</Typography>
-                    </Paper>
-                </Grid>
-                {images.filter(image => image.description === "special").map(({ id, src, title, description }) => (
-                    <Grid item xs={12} sm={4}>
-                        <Box
-                            display="flex"
-                            justifyContent="center"
-                            alignItems="center"
-                            minHeight="20vh"
-                        >
-                            <img key={id} src={src} alt="Logo" className={classes.images} />
-                        </Box>
-                    </Grid>
-                ))}
+                minHeight="20vh"
+              >
+                <img key={id} src={src} alt="Logo" className={classes.images} />
+              </Box>
             </Grid>
-        </div >
-    )
+          ))}
+
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6" align="center" variant="h5">
+              Our Contributory Sponsors
+            </Typography>
+          </Paper>
+        </Grid>
+        {images
+          .filter((image) => image.description === "contributory")
+          .map(({ id, src, title, description }) => (
+            <Grid item xs={12} sm={4}>
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="20vh"
+              >
+                <img key={id} src={src} alt="Logo" className={classes.images} />
+              </Box>
+            </Grid>
+          ))}
+
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography align="center" variant="h5">
+              Our Special Sponsors
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid
+          container
+          direction="coloum"
+          justifyContent="space-around"
+          style={{ paddingTop: "10px" }}
+        >
+          {images
+            .filter((image) => image.description === "special")
+            .map(({ id, src, title, description }) => (
+              <Grid item xs={12} sm={4} alignContent="center">
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  maxWidth="35vh"
+                >
+                  <img
+                    key={id}
+                    src={src}
+                    alt="Logo"
+                    className={classes.images}
+                  />
+                </Box>
+              </Grid>
+            ))}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
--- src/pages/Sponsors/SponsorsLogoList.js
+++ src/pages/Sponsors/SponsorsLogoList.js
@@ -1,15 +1,70 @@
 const images = [
-    { id: 1, src: './images/SponsorLogo/airchina.png', title: 'foo', description: 'valuable' },
-    { id: 2, src: './images/SponsorLogo/hungryPanda.png', title: 'foo', description: 'valuable' },
-    { id: 3, src: './images/SponsorLogo/aPlusAcademy.jpg', title: 'foo', description: 'valuable' },
-    { id: 4, src: './images/SponsorLogo/giga.jpg', title: 'foo', description: 'contributory' },
-    { id: 5, src: './images/SponsorLogo/skyTower.jpg', title: 'foo', description: 'valuable' },
-    { id: 6, src: './images/SponsorLogo/SJTU.jpg', title: 'foo', description: 'valuable' },
-    { id: 7, src: './images/SponsorLogo/statsNZ.jpg', title: 'foo', description: 'special' },
-    { id: 8, src: './images/SponsorLogo/WRSA.png', title: 'foo', description: 'special' },
-    { id: 9, src: './images/SponsorLogo/SUCSA.jpg', title: 'foo', description: 'special' },
-    { id: 10, src: './images/SponsorLogo/NZCAP.jpg', title: 'foo', description: 'special' },
-    { id: 11, src: './images/SponsorLogo/NZEducation.jpg', title: 'foo', description: 'special' },
+  {
+    id: 1,
+    src: "./images/SponsorLogo/airchina.png",
+    title: "foo",
+    description: "valuable",
+  },
+  {
+    id: 2,
+    src: "./images/SponsorLogo/hungryPanda.png",
+    title: "foo",
+    description: "valuable",
+  },
+  {
+    id: 3,
+    src: "./images/SponsorLogo/aPlusAcademy.png",
+    title: "foo",
+    description: "valuable",
+  },
+  {
+    id: 4,
+    src: "./images/SponsorLogo/giga.jpg",
+    title: "foo",
+    description: "contributory",
+  },
+  {
+    id: 5,
+    src: "./images/SponsorLogo/skyTower.png",
+    title: "foo",
+    description: "valuable",
+  },
+  {
+    id: 6,
+    src: "./images/SponsorLogo/SJTU.jpg",
+    title: "foo",
+    description: "valuable",
+  },
+  {
+    id: 7,
+    src: "./images/SponsorLogo/statsNZ.jpg",
+    title: "foo",
+    description: "special",
+  },
+  {
+    id: 8,
+    src: "./images/SponsorLogo/WRSA.png",
+    title: "foo",
+    description: "special",
+  },
+  {
+    id: 9,
+    src: "./images/SponsorLogo/SUCSA.png",
+    title: "foo",
+    description: "special",
+  },
+  {
+    id: 10,
+    src: "./images/SponsorLogo/NZCAP.jpg",
+    title: "foo",
+    description: "special",
+  },
+  {
+    id: 11,
+    src: "./images/SponsorLogo/NZEducation.jpg",
+    title: "foo",
+    description: "special",
+  },
 ];
 
 export default images;
